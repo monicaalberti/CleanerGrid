f37761461db0c875105efef7378927bfb8d4bcc5
mengyinggg <146138497+mengyinggg@users.noreply.github.com>
2023-11-14 19:29:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,70 @@ app.config["SECRET_KEY"]= "this-isn't-my-secret-key"
 app.config["SESSION_PERMANENT"]=False
 app.config["SESSION_TYPE"]="filesystem"
 Session(app)
+
+
+@app.before_request
+def logged_in_user():
+
+def login_required(view):
+    @wraps(view)
+    def wrapped_view(*args, **kwargs):
+        if g.user is None:
+            return redirect(url_for("login",next=request.url) )
+        return view(*args, **kwargs)
+    return wrapped_view
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/register",methods=["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user_id= form.user_id.data
+        password = form.password.data
+        password2 = form.password2.data
+        db=get_db()
+        possible_clashing_user = db.execute("""SELECT *
+                                            FROM user
+                                            WHERE user_id=?;""",(user_id,)).fetchone()
+        if possible_clashing_user is not None:
+            form.user_id.errors.append("User is already taken!")
+        else:
+            db.execute("""INSERT INTO user(user_id,password)
+                       VALUES(?,?); """,
+                       (user_id,generate_password_hash(password)))
+            db.commit()
+            return redirect( url_for("login"))
+    return render_template("register.html",form=form)
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    form = UserLoginForm()
+    if form.validate_on_submit():
+        user_id= form.user_id.data
+        password= form.password.data
+        db= get_db()
+        possible_clashing_user = db.execute("""SELECT *
+                                            FROM user
+                                            WHERE user_id = ?;""",
+                                            (user_id,)).fetchone()
+        if possible_clashing_user is None:
+            form.user_id.errors.append("Account is not exists! ")
+        elif not check_password_hash(possible_clashing_user["password"],password):
+            form.user_id.errors.append("Incorrect password!")
+        else:
+            session.clear()
+            session["user_id"] = user_id
+            next_page= request.args.get("index")
+            if not next_page:
+                next_page = url_for("index")
+            return redirect(next_page)
+    return render_template("login.html",form=form)
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect( url_for("index") )
+
